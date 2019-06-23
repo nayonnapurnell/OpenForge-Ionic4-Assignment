ee56bcff7634792b928877680ef2d06b51168b86
“Nayonna <nayonnapurnell@outlook.com>
2019-06-22 23:37:18 -0400

    Added the App routes to retrieve Github users

---
+++ OpenForge-Ionic4-Assignment/src/app/pages/github-user-details/github-user-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-user-details',
+  templateUrl: './github-user-details.page.html',
+  styleUrls: ['./github-user-details.page.scss'],
+})
+export class GithubUserDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
