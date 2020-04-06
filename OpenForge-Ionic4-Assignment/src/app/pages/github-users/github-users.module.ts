5510f71ca2e02c3e6440b9c32f45ffd4b32eecd7
“Nayonna <Nayonna Purnell>
2020-04-06 06:57:39 -0400

    Added modified files from security alert

--- OpenForge-Ionic4-Assignment/src/app/pages/github-users/github-users.module.ts
+++ OpenForge-Ionic4-Assignment/src/app/pages/github-users/github-users.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 import { IonicModule } from '@ionic/angular';
 
--- OpenForge-Ionic4-Assignment/src/app/services/github-user.service.ts
+++ OpenForge-Ionic4-Assignment/src/app/services/github-user.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType {
+  all = '',
+  name = 'monalisa octocat',
+  avatar_url = 'https://github.com/images/error/octocat_happy.gif',
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubUserService {
+  url = 'https://api.github.com/users?since=135';
+  apiKey = ''; // <-- Enter your own key here!
+ 
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  searchData(login: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(login)}&type=${type}&apikey=${this.apiKey}`).pipe(
+      map(results => results['Search'])
+    );
+  }
+
+  getDetails(id) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
 }
